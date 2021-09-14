96be91a9e8b619d80fa4130d645fd282690e8ae2
Ambar Mutha <ambar.ytl@gmail.com>
2021-09-14 17:06:31 +0530

    commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { NotificationDisplay, notifier } from '@beyonk/svelte-notifications';
+
+	function someFunction() {
+		notifier.success('Notifications work!', 5000);
+		console.log('click');
+	}
+</script>
+
+<NotificationDisplay />
+
+<button on:click={someFunction}>Show message</button>
